394849bcdee565f723a361235ad2024a04ff70a3
gwerrry <tunstullcarlos@gmail.com>
2021-07-28 00:27:45 -0400

    change version

--- src/main/java/net/breakfaststudios/BreakfastSounds.java
+++ src/main/java/net/breakfaststudios/BreakfastSounds.java
@@ -25,7 +25,7 @@ public class BreakfastSounds {
      * //TODO For every release make sure this is changed. It should correspond to the github tag for the release.
      *
      */
-    public static final String currentVersion = "v1.2";
+    public static final String currentVersion = "v1.3";
     protected static final String os = Util.os;
     public static String SELECTED_AUDIO_DEVICE;
     private static SoundBoard soundBoard;
